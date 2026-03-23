764d9fa3a14c448fb718b753b1002cce49a2a885
machangbao <1443786299@qq.com>
2026-03-23 19:04:23 +0800

    ssss

---
+++ api/dashscope/[...path].js
@@ -0,0 +1,43 @@
+const https = require('https')
+
+module.exports = function handler(req, res) {
+  const url = new URL(req.url || '/', 'http://localhost')
+  const prefix = '/api/dashscope'
+  if (!url.pathname.startsWith(prefix)) {
+    res.status(404).json({ error: 'not found' })
+    return
+  }
+  let pathAfter = url.pathname.slice(prefix.length)
+  if (!pathAfter || pathAfter === '') pathAfter = '/'
+  else if (!pathAfter.startsWith('/')) pathAfter = '/' + pathAfter
+  const upstreamPath = pathAfter + url.search
+
+  const headers = {}
+  for (const [key, value] of Object.entries(req.headers)) {
+    const k = key.toLowerCase()
+    if (!['host', 'connection'].includes(k)) headers[key] = value
+  }
+  headers.host = 'dashscope.aliyuncs.com'
+
+  const options = {
+    hostname: 'dashscope.aliyuncs.com',
+    path: upstreamPath,
+    method: req.method,
+    headers,
+  }
+
+  const proxy = https.request(options, (upstream) => {
+    res.status(upstream.statusCode)
+    for (const [key, value] of Object.entries(upstream.headers)) {
+      if (key.toLowerCase() !== 'transfer-encoding') res.setHeader(key, value)
+    }
+    upstream.pipe(res)
+  })
+
+  proxy.on('error', (err) => res.status(502).json({ error: err.message }))
+  req.pipe(proxy)
+}
+
+module.exports.config = {
+  api: { bodyParser: false },
+}
